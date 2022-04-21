3474fcaaf86dc016dcff635ebe6a6f225aa62fe9
Fagner Schwalm <fagnerschwalm@gmail.com>
2022-04-21 19:34:45 -0300

    simulate setState with an object OR function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,24 +3,36 @@
 
 import * as React from 'react'
 
+const isFunction = value =>
+  value &&
+  (Object.prototype.toString.call(value) === '[object Function]' ||
+    'function' === typeof value ||
+    value instanceof Function)
 
-function StateReducer(previousState, newState) {
+function StateReducer(currentState, action = {}) {
+  if (isFunction(action)) {
+    return {...currentState, ...action(currentState)}
+  }
+
   return {
-    ...previousState,
-    ...newState
-  };
+    ...currentState,
+    ...action,
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(StateReducer, {count: initialCount })
+  const [state, setState] = React.useReducer(StateReducer, {
+    count: initialCount,
+  })
 
-  const {count} = state;
+  const {count} = state
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
